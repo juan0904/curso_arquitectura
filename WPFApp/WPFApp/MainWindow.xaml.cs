21f24e481bde90dddca07a3632e480bdca501ee3
Juan Jose Sanchez <juanjsanchezcea@gmail.com>
2024-10-14 18:10:33 -0500

    Finalizacion de proyecto de WPF y tasks

--- WPFApp/WPFApp/MainWindow.xaml.cs
+++ WPFApp/WPFApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,5 +26,52 @@ namespace WPFApp
 			InitializeComponent();
 		}
 
-	}	
+		void AddMessage(string message)
+		{
+			this.Dispatcher.Invoke(() =>
+			{
+				Messages.Content +=
+				$"Mensaje: {message}" +
+				$"Hilo social: {Thread.CurrentThread.ManagedThreadId}";
+			});
+		}
+
+		void CreateTask()
+		{
+			Task T;
+			Action Code = new Action(ShowMesage);
+			T = new Task(Code);
+			Task T2 = new Task(delegate
+			{
+				MessageBox.Show("Ejecutando una tarea en un metodo anonimo");
+			});
+			Task T3A = new Task(ShowMesage);
+			Task T3 = new Task(
+				() => ShowMesage());
+
+			Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
+
+			Task T5 = new Task(() =>
+			{
+				DateTime CurrentDate = DateTime.Today;
+				DateTime StartDate = CurrentDate.AddDays(30);
+				MessageBox.Show($"Tarea 5. Fecha Calculado {StartDate}");
+			});
+
+			Task T6 = new Task((message) =>
+			MessageBox.Show(message.ToString()), "Expresion lambda con parametros");
+
+			Task T7 = new Task(() => AddMessage("Ejecutando la tarea"));
+			T7.Start();
+
+			AddMessage("En el hilo principal");
+		}
+
+		private void ShowMesage()
+		{
+			MessageBox.Show("Ejecutando el metodo ShowMesage");
+		}
+
+
+	}
 }
